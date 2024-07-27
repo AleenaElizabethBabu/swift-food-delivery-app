873153ab7cd4357ef8027295d7156d6bb4fefea4
AmalMathew10 <158222372+AmalMathew10@users.noreply.github.com>
2024-07-27 17:49:29 +0000

    list

--- src/components/RstaurantList.js
+++ src/components/RstaurantList.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const RestaurantList = () => {
-    return (
-        <div>
-            <h2>Restaurants</h2>
-            {/* List of restaurants goes here */}
-        </div>
-    );
-};
-
-export default RestaurantList;
