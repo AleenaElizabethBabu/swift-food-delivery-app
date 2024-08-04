0da7ba4e1d06f4fb937d0eb6365a9cd941c8c22c
AmalMathew10 <158222372+AmalMathew10@users.noreply.github.com>
2024-08-04 02:17:07 +0000

    Added a new navigation link named contack

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,7 @@ const Navigation = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/restaurants">Restaurants</Link></li>
                 <li><Link to="/order">Order</Link></li>
+                <li><Link to="/contact">Contact</Link></li> 
             </ul>
         </nav>
     );
